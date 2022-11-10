c3eb61cf204375c41849cd236ef3c54633e54a1d
sharif291 <talha.halcyon@gmail.com>
2022-11-10 17:58:23 +0600

    add some code

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -1,11 +1,30 @@
+import axios from 'axios';
 import React from 'react';
 import { Table } from 'react-bootstrap';
 import { useQuery } from 'react-query';
+import { useSelector } from 'react-redux';
 import { allProducts } from '../api/fetchData';
+import { alrtError, alrtSuccess } from '../utils/common';
 import Loading from './Loading';
 
 export default function ProductTable() {
+  const { token } = useSelector((state) => state.auth.user);
   const { data, isLoading } = useQuery('allProducts', () => allProducts());
+
+  const deleteProduct = async (key) => {
+    const confirm = window.confirm('Are you sure want to delete?');
+
+    if (confirm) {
+      await axios
+        .delete(`${process.env.REACT_APP_PROXY}/api/v1/products/${key}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => alrtSuccess(res.data.message))
+        .catch((error) => alrtError(error.message));
+    }
+  };
   return (
     <>
       {isLoading ? (
@@ -14,6 +33,7 @@ export default function ProductTable() {
         <Table hover striped>
           <thead>
             <tr className="bg-dark text-light">
+              <th>SI NO</th>
               <th>Product ID</th>
               <th>Product Name</th>
               <th>Tire Code</th>
@@ -23,25 +43,31 @@ export default function ProductTable() {
               <th>Price</th>
               <th>Inside Charge</th>
               <th>Outside Charge</th>
-              <th>Action</th>
+              <th className="text-center">Action</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>105</td>
-              <td>CST Jack Rabbit </td>
-              <td>C1747</td>
-              <td>26x2.10</td>
-              <td>bi-cycle</td>
-              <td>6</td>
-              <td>Price</td>
-              <td>Inside Charge</td>
-              <td>Outside Charge</td>
-              <td>
-                <button>Edit</button>
-                <button>Show</button>
-              </td>
-            </tr>
+            {data.map((product, i) => (
+              <tr key={product._id}>
+                <td>{i + 1}</td>
+                <td>{product._id}</td>
+                <td>{product.title} </td>
+                <td>{product.code}</td>
+                <td>{product.size}</td>
+                <td>{product.categories}</td>
+                <td>{product.stockQuantity}</td>
+                <td>{product.price}</td>
+                <td>{product.shippingInDhaka}</td>
+                <td>{product.shippingInDhaka}</td>
+                <td>
+                  <button>Edit</button>
+                  <button>Show</button>
+                  <button onClick={() => deleteProduct(product._id)}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
       )}
